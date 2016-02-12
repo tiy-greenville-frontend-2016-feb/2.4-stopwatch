ad8d3682c6b02611f2d613a0f82b6cd40c99ae5d
Dan Dietz <dietz@Dans-MacBook-Pro.local>
2016-02-12 11:40:01 -0500

    start button gets counter going.

--- main.js
+++ main.js
@@ -1,31 +1,64 @@
 (function(){
   'use strict';
 
-  var myStartButton;
-
-  console.log(document);
-
-  myStartButton = document.querySelector('.start');
+  var startTime;
+  var myStartButton = document.getElementById('start-button');
+  var hours = document.querySelector('.elapsed-time .hours');
+  var minutes = document.querySelector('.elapsed-time .minutes');
+  var seconds = document.querySelector('.elapsed-time .seconds');
 
   console.log(myStartButton);
 
-  /*
-    Function to handle button clicks
-  */
+  function toggleButton(){
+      myStartButton.style.backgroundColor = 'red';
+      myStartButton.textContent = 'Stop';
+  }
+
+  function milliscondsToInterval(ms){
+    var msLeft = ms;
+
+    var msInHour = 3600000;
+    var msInMin = 60000;
+    var msInSec = 1000;
+
+    var hours = Math.floor(msLeft / msInHour);
+    msLeft = msLeft % msInHour;
+
+    var minutes = Math.floor(msLeft / msInMin);
+    msLeft = msLeft % msInMin;
+
+    var seconds = Math.floor(msLeft / msInSec);
+
+    return [hours, minutes, seconds];
+  }
+
+  function formatInterval(interval) {
+    var formatted = [];
+    formatted[0] = ("0" + interval[0]).slice(-2);
+    formatted[1] = ("0" + interval[1]).slice(-2);
+    formatted[2] = ("0" + interval[2]).slice(-2);
+    return formatted;
+  }
+
+  function displayInterval(interval) {
+    var formattedInterval = formatInterval(interval);
+    hours.textContent = formattedInterval[0];
+    minutes.textContent = formattedInterval[1];
+    seconds.textContent = formattedInterval[2];
+  }
+
+  function logElapsedTime(){
+    var currentTime = new Date();
+    var elapsedTime = currentTime - startTime;
+    var elapsed = milliscondsToInterval(elapsedTime);
+
+    displayInterval(elapsed);
+  }
+
   function startButtonHandler(event){
-    console.log('I was just clicked!!');
-    var startButton = document.querySelector('.start');
-    startButton.style.backgroundColor = 'red';
-    startButton.textContent = 'Stop';
-    //alert(startButton.textContent);
-
-    var pageTitle = document.querySelector('.page-title');
-    console.log(pageTitle.textContent);
-
-    pageTitle.textContent = 'Stopwatch';
-    console.log(pageTitle.textContent);
-
-    console.log(pageTitle.id);
+    startTime = new Date();
+    toggleButton();
+    window.setInterval(logElapsedTime, 1000);
   }
 
   myStartButton.addEventListener('click', startButtonHandler);
